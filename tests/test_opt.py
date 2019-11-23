b2537ab3c9c5655c3f9e25514f272966325fa985
SeparateRecords <SeparateRecords@users.noreply.github.com>
2019-11-24 02:00:08 +1100

    Fix broken tests

--- tests/test_opt.py
+++ tests/test_opt.py
@@ -42,10 +42,9 @@ def test_take_args_valid_but_no_args_found_after_option():
     assert args == ['a', 'b', '-c']
 
 
-@pytest.mark.parametrize("greedy_op", (..., any, lethargy.greedy, "*"))
-def test_take_args_greedy_returns_everything(args, greedy_op):
+def test_take_args_greedy_returns_everything(args):
     expected = ["b", "c", "-d", "e", "--fgh", "i", "j", "-xyz"]
-    assert Opt("a").takes(greedy_op).take_args(args) == expected
+    assert Opt("a").takes(...).take_args(args) == expected
     assert args == []
 
 
@@ -66,13 +65,13 @@ def test_take_args_not_found_default_none_greedy_returns_empty_list(args):
 
 
 # opt not found, default not none, raises false
-@pytest.mark.parametrize("amt", (1, 2, *lethargy.GREEDY_VALUES))
+@pytest.mark.parametrize("amt", (1, 2, ...))
 def test_take_args_not_found_default_not_none_returns_default(args, amt):
     assert Opt("w").takes(amt).take_args(args, default=1) == 1
 
 
 # opt not found, default not none, raises true
-@pytest.mark.parametrize("amt", (1, 2, *lethargy.GREEDY_VALUES))
+@pytest.mark.parametrize("amt", (1, 2, ...))
 def test_take_args_not_found_raises_true_raises_missingoption(args, amt):
     with pytest.raises(lethargy.MissingOption):
         assert Opt("w").takes(amt).take_args(args, raises=True)
