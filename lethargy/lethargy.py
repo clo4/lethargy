513f3634c24dc0a4a7fce8c057c22ebd1d3957c6
SeparateRecords <52195359+SeparateRecords@users.noreply.github.com>
2020-03-16 21:24:39 +1100

    Sinplify public interface (renames and removals)
    * Remove Opt.__iter__ (unnecessary)
    * Rename Opt.find_in to Opt._find_in
    * Rename Opt._amount to Opt._argc

--- lethargy/lethargy.py
+++ lethargy/lethargy.py
@@ -60,17 +60,14 @@ class Opt:
     """Define an option to take it from a list of arguments."""
 
     def __init__(self, *names: str):
-        self._names = {*map(stab, names)} if names else set()
-        self._amount = 0
+        self._names = {stab(name) for name in names}
+        self._argc = 0
         self._tfm = identity
-
-    def __iter__(self):
-        return iter(self._names)
 
     def __copy__(self):
         new = self.__class__()
         new._names = copy(self._names)
-        new._amount = self._amount
+        new._argc = self._argc
         new._tfm = self._tfm
         return new
 
@@ -85,10 +82,10 @@ class Opt:
         else:
             metavar = self._tfm.__name__.lower()
 
-        if is_greedy(self._amount):
+        if is_greedy(self._argc):
             vals = f"[{metavar}]..."
-        elif self._amount > 0:
-            vals = " ".join([f"<{metavar}>"] * self._amount)
+        elif self._argc > 0:
+            vals = " ".join([f"<{metavar}>"] * self._argc)
         else:
             return names
 
@@ -107,8 +104,8 @@ class Opt:
         # This whole thing is optional, if there's nothing to show it won't
         # be in the repr string.
         # Should try to be smart about representing the converter.
-        if self._amount != 0 or self._tfm is not None:
-            takes = [self._amount]
+        if self._argc != 0 or self._tfm is not None:
+            takes = [self._argc]
             if self._tfm is not identity:
                 if isinstance(self._tfm, type):
                     takes.append(self._tfm.__name__)
@@ -125,11 +122,20 @@ class Opt:
         try:
             return (
                 self._names == other._names
-                and self._amount == other._amount
+                and self._argc == other._argc
                 and self._tfm == other._tfm
             )
         except AttributeError:
             return NotImplemented
+
+    def _find_in(self, args):
+        """Search args for this option and return an index if it's found."""
+        for name in self._names:
+            try:
+                return args.index(name)
+            except ValueError:
+                continue
+        return None
 
     def takes(self, n, tfm=None):
         """Set the number of arguments the instance takes."""
@@ -137,23 +143,14 @@ class Opt:
             msg = f"The number of arguments ({n}) must be positive or greedy ('...')"
             raise ArgsError(msg)
 
-        self._amount = n
+        self._argc = n
         if tfm is not None:
             self._tfm = tfm
         return self
 
-    def find_in(self, args):
-        """Search args for this option and return an index if it's found."""
-        for name in self:
-            try:
-                return args.index(name)
-            except ValueError:
-                continue
-        return None
-
     def take_flag(self, args=argv, *, mut=True):
         """Get a bool indicating whether the option was present in the arguments."""
-        idx = self.find_in(args)
+        idx = self._find_in(args)
         if idx is None:
             return False
         if mut:
@@ -162,7 +159,7 @@ class Opt:
 
     def take_args(self, args=argv, *, d=None, raises=False, mut=True):
         """Get the values of this option."""
-        amt = self._amount
+        amt = self._argc
 
         # Taking less than 1 argument will do nothing, better to use take_flag
         if isinstance(amt, int) and amt < 1:
@@ -170,7 +167,7 @@ class Opt:
             raise ArgsError(msg.format(self, amt))
 
         # Is this option in the list?
-        index = self.find_in(args)
+        index = self._find_in(args)
 
         # Option not found in args, skip the remaining logic and return the
         # default value. No list mutation will occur
@@ -216,8 +213,9 @@ class Opt:
             # Single value if amt is 1
             return self._tfm(taken[0])
 
-        if is_greedy(amt) or amt > 1:  # Short circuit if greedy
-            # List of values (`taken` will always be a list)
+        # Short circuits if greedy to prevent ``... > 1``
+        if is_greedy(amt) or amt > 1:
+            # List of values (`taken` will always be iterable)
             return [self._tfm(x) for x in taken]
 
         # amt is (somehow) invalid -- manually set to a negative value?
