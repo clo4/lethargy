db7fb66ae1d7d048cd7147d0022ce3587311c12a
SeparateRecords <52195359+SeparateRecords@users.noreply.github.com>
2020-05-02 11:53:46 +1000

    Add some more tests

--- tests/options/test_variadic.py
+++ tests/options/test_variadic.py
@@ -1,12 +1,45 @@
-"""
-## What needs the be tested?
+# Keep on separate lines for better diff and readability ~
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=protected-access
+# pylint: disable=redefined-outer-name
 
-- [ ] init
-- [ ] string shows appropriate metavar
-- [ ] found transforms each argument
-- [ ] found always returns list
-- [ ] missing always returns empty list
-- [ ] span returns the start index and no end index
-- [ ] span raises indexerror if option is not in args
+import pytest
 
-"""
+from lethargy.options import Variadic as Var
+
+identity = lambda x: x
+
+
+def test_init():
+    v = Var("Name", float)
+    assert v.names == "Name"
+    assert v._transform is float
+
+
+def test_str():
+    assert str(Var(["-v", "--variadic"], int)) == "-v|--variadic [int]..."
+    assert str(Var(["-v", "--variadic"], identity)) == "-v|--variadic [value]..."
+
+
+def test_missing_is_always_empty_list():
+    assert Var.missing(None) == []
+
+
+def test_found_transforms_each_argument_after_first():
+    rv = Var((), int).found(["x", "1", "2", "3", "4"])
+    assert rv == [1, 2, 3, 4]
+
+
+def test_span():
+    thing_to_find = "e"
+    args = list("abcdefghijklmnopqrstuvwxyz")
+    expected = list("efghijklmnopqrstuvwxyz")
+    start, end = Var(thing_to_find, identity).span(args)
+    assert args[start:end] == expected
+
+
+def test_span_raises_indexerror_if_target_is_not_in_list():
+    with pytest.raises(IndexError):
+        Var((), identity).span(())
